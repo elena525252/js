9a031a1c194ac20a6ac6c4fc0d21d677c1433637
elena525252 <90596047+elena525252@users.noreply.github.com>
2022-03-25 10:40:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ let servicePrice1 = prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = prompt("Сколько это будет стоить?");
 let fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
-let servicePercentPrice = Number(fullPrice) - Number(rollback);
+let servicePercentPrice;
+let allServicePrices;
 
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+}
 if (fullPrice >= 30000) {
     console.log("Даем скидку в 10%")
 } else if (fullPrice >= 15000 && fullPrice <= 30000) {
@@ -19,17 +23,42 @@ if (fullPrice >= 30000) {
 } else if (fullPrice <= 0) {
     console.log("Что-то пошло не так");
 }
+const getAllServicePrices = function(servicePrice1, servicePrice2) {
+    return (servicePrice1 + servicePrice2)
+}
+allServicePrices = getAllServicePrices(+servicePrice1, +servicePrice2);
 
+function getFullPrice(screenPrice, allServicePrices) {
+    return (screenPrice + allServicePrices)
+}
+fullPrice = getFullPrice(+screenPrice, +allServicePrices);
 
-/*console.log(title);
-console.log(fullPrice);
+const getTitle = function() {
+    return title.replace(/(^|\s)\S/g, function(a) {return a.toUpperCase()})
+}
+title = getTitle();
+
+function getServicePercentPrice(fullPrice, rollback) {
+    return (fullPrice - rollback)
+}
+servicePercentPrice = getServicePercentPrice(+fullPrice, +rollback);
+
+showTypeOf(title)
+showTypeOf(allServicePrices)
+
+showTypeOf(servicePercentPrice)
+//console.log(allServicePrices);
+//console.log(title);
+/*
 console.log(adaptive);
 console.log(screens.length);
 console.log(screenPrice + " " + "рублей");
-console.log(fullPrice + " " + "рублей");
+*/
 console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback/100));*/
-console.log(servicePercentPrice)
+//console.log(fullPrice * (rollback/100));
+//console.log(fullPrice + " " + "рублей");
+//console.log(servicePercentPrice);
+
 
 /*alert ("Hello world!");
 console.log("Hello world!");*/
