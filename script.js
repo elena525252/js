d65641152aa4ba72792e8108b4e1f9fbe76ea33a
elena525252 <90596047+elena525252@users.noreply.github.com>
2022-03-25 10:38:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,5 +4,22 @@ let screenPrice;
 let rollback;
 let fullPrice;
 let adaptive;
-alert ("Hello world!")
-console.log("Hello world!")
+
+title = "idk";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 5000;
+rollback = 50;
+fullPrice = 10000;
+adaptive = true;
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+console.log(screenPrice + " " + "рублей");
+console.log(fullPrice + " " + "рублей");
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback/100));
+
+//alert ("Hello world!");
+console.log("Hello world!");
