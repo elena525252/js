3bb036428f4d44d4f5ccab39448b99c84e88af73
elena525252 <90596047+elena525252@users.noreply.github.com>
2022-03-31 11:45:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
-alert ("Hello world!")
-console.log("Hello world!")
+'use strict';
+
+const btn = document.getElementById('btn')
+let square = document.getElementById('square')
+let input = document.getElementById('text')
+let e_btn = document.getElementById('e_btn')
+let range = document.getElementById('range')
+let circle = document.getElementById('circle')
+let span = document.getElementById('range-span')
+
+
+const color = function () {
+        square.style.backgroundColor = input.value  
+}
+
+btn.addEventListener('click', () => {
+    color()
+})
+
+const invisible = function () {
+    e_btn.style.display = 'none'
+}
+
+e_btn.addEventListener('click', () => {
+    invisible()
+})
+
+const target = function (event) {
+    span.textContent = event.target.value
+}
+
+range.addEventListener('change', target)
+
+const hw = function () {
+    circle.style.height = range.value + '%'
+    circle.style.width = range.value + '%'
+}
+
+range.addEventListener('input', hw)
+
+console.dir(circle)
